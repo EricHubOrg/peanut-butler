152e7c1f95ebb717e0d55b59278aa0ebffe1be31
Eric Lopez <pikurrot@gmail.com>
2023-07-28 18:04:40 +0200

    Create llama.py

---
+++ llama.py
@@ -0,0 +1,9 @@
+from transformers import AutoTokenizer, AutoModelForCausalLM
+
+tokenizer = AutoTokenizer.from_pretrained("/llama2")
+model = AutoModelForCausalLM.from_pretrained("/llama2")
+
+def generate(prompt, max_length=50):
+	inputs = tokenizer(prompt, return_tensors="pt")
+	generate_ids = model.generate(inputs.input_ids, max_length=max_length)
+	return tokenizer.batch_decode(generate_ids, skip_special_tokens=True, clean_up_tokenization_spaces=False)[0]
